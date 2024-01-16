565421a63f2005936ac68483a9510773373f89ae
DimaPFf <set1304@gmail.com>
2024-01-16 20:45:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,138 @@
+// let a = 12
+// console.log('1 = ', a)
+// a = 13
+
+// console.log('2 = ', a)
+
+// const b = 14
+
+// console.log('b 1 = ', b)
+// b = 15
+
+// console.log('b 2 = ', b)
+
+// const name = 'Den'
+
+// console.log('name = ', name)
+
+// console.log('c = ', c)
+
+// var c = 'test'
+// console.log('c = ', c)
+// c = 'test2'
+// console.log('c = ', c)
+
+// bigint
+// const bigNum = 123456789n
+
+// const sameBigNum = BigInt('123456789')
+
+// console.log('bigNum', bigNum)
+// console.log('sameBigNum', sameBigNum)
+
+// String 
+
+// const str = 'string'
+
+// const str2 = 'string "string" string'
+
+// const name = 'Alex'
+
+// const surname = 'Petrov'
+
+// const str3 = `Hello ${name} ${surname}`
+
+// console.log('str' , str)
+// console.log("str2", str2)
+
+// console.log(str3)
+
+// Boolean
+
+// const flag = true
+
+// const flag2 = false
+
+// console.log(flag, flag2)
+
+// Symbol
+
+// const smbl = Symbol('id')
+// const smbl2 = Symbol('id')
+
+// console.log(smbl)
+
+
+//Object
+
+// const obj = { name: 'Dima', surname: 'Petrov'} // key: value
+
+// console.log(`Hello ${obj.name} ${obj.surname}`)
+
+// let a = 3
+
+// let b = a
+
+// b = 4
+
+// console.log(b) // 4
+// console.log(a) // 3
+
+
+// const obj = { name: 'Dima', surname: 'Petrov'}
+
+// const otherObj = obj
+
+// const otherObj2 = obj
+
+// otherObj.name = 'Denis'
+
+// console.log("obj", obj) // { name: 'Dima', surname: 'Petrov'}
+// console.log("otherObj", otherObj) // { name: 'Denis', surname: 'Petrov'}
+
+// Operators
+
+// console.log('+', 2 + 2) 
+// console.log('-', 2 - 2) 
+// console.log('*', 2 * 2) 
+// console.log('/', 2 / 2) 
+// console.log('**', 2 ** 2)
+
+// console.log('+', 2 + '2') 
+
+// console.log('+','Hello' + ' Dima') 
+
+// console.log('-', 'Hello'  - 1) // NaN
+
+// console.log('+', true  + true)
+
+// console.log('+', {}  + true) //[ObjectObjet]true
+
+// console.log('+', '' + 1) // 
+
+// const a = 3
+
+// console.log(a == 4)
+
+// console.log('' === 0)
+
+// console.log(true == {})
+
+// console.log(23 > 21) // true
+// console.log(23 >= 23) // true
+// console.log(23 < 25) // true
+
+// console.log(!(23 > 23))
+
+// console.log(87 % 10)
+
+
+// alert("Hello human!")
+// const name = prompt('name?')
+// console.log(name)
+// const flag = confirm('Agree?')
+// console.log(flag)
+
+const value = null
+
+console.log(typeof value)
