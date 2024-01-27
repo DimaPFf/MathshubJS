df661b7a9fa719cfcbd645cbecb409d5044afdfe
krasnik095 <artem.krasnik@mail.ru>
2024-01-28 01:53:07 +0300

    Lesson-2

--- lesson2/hw/Artem/script.js
+++ lesson2/hw/Artem/script.js
@@ -1,3 +1,10 @@
+console.log('1)', -1 && 0 && 'false')
+console.log('2)', +'0' || '0' || false)
+console.log('3)', !!'false' || 0 + '' || 29)
+console.log('4)', "1"**2 && 9 - '9' && null)
+console.log('5)', undefined || null || +null || !undefined)
+
+
 
 const a = prompt ("Угадай мой возвраст");
 
